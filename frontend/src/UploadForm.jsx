05ea4be17a26ff2ea57e35a1800249f2bd6e8150
Tarun Saini <tarunsaini532@gmail.com>
2026-03-11 19:45:12 +0530

    update frontend API URL

--- frontend/src/UploadForm.jsx
+++ frontend/src/UploadForm.jsx
@@ -20,10 +20,7 @@ export default function UploadForm() {
     try {
       setLoading(true);
 
-      const res = await axios.post(
-        "http://localhost:5000/api/upload",
-        formData,
-      );
+      const res = axios.post(`${import.meta.env.VITE_API_URL}/api/upload`, formData);
 
       setMessage(res.data.message);
     } catch (err) {
